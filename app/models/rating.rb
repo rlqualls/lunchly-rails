7ef6b91e49769974ba6ef9e26d993f85c2b05a8d
Robert Qualls <robert@robertqualls.com>
2016-02-27 17:34:51 -0600

    Validate rating score 0 to 5

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
+
+  validates :score, inclusion: { in: (0..5), message: "Ratings are 0 to 5" }
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Rating, :type => :model do
   it 'has a valid factory' do
     expect(build(:rating)).to be_valid
   end
+
+  it 'can have a score of 0' do
+    rating = build(:rating)
+    rating.score = 0
+    expect(rating).to be_valid
+  end
+
+  it 'can have a score of 5' do
+    rating = build(:rating)
+    rating.score = 5
+    expect(rating).to be_valid
+  end
+
+  it 'cannot have a score of -1' do
+    rating = build(:rating)
+    rating.score = -1
+    expect(rating).to be_invalid
+  end
+
+  it 'cannot have a score of 6' do
+    rating = build(:rating)
+    rating.score = 6
+    expect(rating).to be_invalid
+  end
 end
