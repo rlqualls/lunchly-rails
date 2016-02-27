370a5c3e74097570c66def9af7bd9f9d56f8faa1
Robert Qualls <robert@robertqualls.com>
2016-02-27 17:00:20 -0600

    Pass factory specs

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :restaurant do
-    title "MyString"
-    description "MyText"
-    address "MyText"
+    title "Restaurant Title"
+    description "Restaurant Description"
+    address "1001 Streeties Lane, City, ST"
   end
 end
