20440cd0ff09c94b15030c19041a8805326b48f7
Robert Qualls <robert@robertqualls.com>
2016-02-28 04:01:27 -0600

    Add ability to delete restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,10 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find_by_id params["id"]
   end
+
+  def destroy
+    @restaurant = Restaurant.find_by_id params["id"]
+    @restaurant.destroy
+    redirect_to :back
+  end
 end
