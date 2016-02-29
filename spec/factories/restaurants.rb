0bf14c6abf62d2ea0fc6b8c8694c2c0c6145c923
Robert Qualls <robert@robertqualls.com>
2016-02-29 08:48:53 -0600

    Add nav icons to restaurant show view
    1. Also use shorter title in factory for seeds

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -2,7 +2,7 @@ require_relative "../support/factory_girl/factory_helpers"
 
 FactoryGirl.define do
   factory :restaurant do
-    title { "#{FFaker::Name.name}'s #{FFaker::Food.herb_or_spice}" }
+    title { "#{FFaker::Name.first_name}'s #{FFaker::Food.herb_or_spice}" }
     description { "Restaurant Description" }
     address { "#{FFaker::Address.street_address}, City, ST" }
     last_visited { random_time_in_past }
