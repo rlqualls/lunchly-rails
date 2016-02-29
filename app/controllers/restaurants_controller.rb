3925538737553627739eb51aff55a695148fd348
Robert Qualls <robert@robertqualls.com>
2016-02-29 11:57:19 -0600

    Add ability edit restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,28 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find_by_id params["id"]
   end
 
+  def edit
+    @restaurant = Restaurant.find_by_id params["id"]
+  end
+
+  def update
+    @restaurant = Restaurant.find_by_id params["id"]
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @restaurant = Restaurant.find_by_id params["id"]
     @restaurant.destroy
-    redirect_to :back
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:title, :description, :phone_number)
   end
 end
